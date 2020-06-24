57d94f409e25960378345b6d3e659de0ffaa9b28
Guneet9 <64691372+Guneet9@users.noreply.github.com>
2020-06-24 21:29:18 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,22 @@
+library(shiny)
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Cars Dataset - Miles per Gallon"),
+  
+  sidebarPanel(
+    
+    selectInput("variable", "Variable:", 
+                c("Cylinders" = "cyl",
+                  "Transmission" = "am",
+                  "Gears" = "gear"))
+  ),
+  
+  mainPanel(
+    # Output: Formatted text for caption ----
+    h3(textOutput("caption")),
+    
+    # Output: Plot of the requested variable against mpg ----
+    plotOutput("mpgPlot")
+  )
+))
